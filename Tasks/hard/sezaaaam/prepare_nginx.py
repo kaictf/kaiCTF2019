d852f499fd75783b7748776158b99fa236db6c89
Kirill Kukarcev <kirillkukarcev@gmail.com>
2019-05-07 23:12:26 +0300

    Sezaam. Working, but flag is available under LVL1.
    But it can be a feature, because, you don't have a clue of how to enter the flag directory, you need to search it manually.

---
+++ Tasks/hard/sezaaaam/prepare_nginx.py
@@ -0,0 +1,37 @@
+#!/bin/python
+
+
+template = """location ~ ^<PATH>$ {
+        autoindex on;
+        auth_basic           'LVL<INT>';
+        auth_basic_user_file /var/www/levels/level<INT>;
+    }"""
+
+
+levels = 10
+start_path = "\/\w*\/"
+
+aggregated_locations = []
+
+for r in range(2,levels+1):
+
+    # Put PATH into template
+    prepared_template = template.replace("<PATH>",start_path)
+
+    # Change level number
+    prepared_template = prepared_template.replace("<INT>", str(r))
+
+    aggregated_locations.append(prepared_template + '\n')
+
+    start_path += '\w*\/'
+
+
+with open('sezaam.conf.tmp') as file:
+    nginx_template = file.read()
+
+    final_config = nginx_template.replace("<LOCATIONS>", ''.join(reversed(aggregated_locations)))
+
+
+with open('sezaam.conf','w') as file:
+    file.write(final_config)
+

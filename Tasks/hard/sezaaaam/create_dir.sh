2c83fb369346a8e43655327add49b353b5507eca
Kirill Kukarcev <kirillkukarcev@gmail.com>
2019-05-08 18:14:57 +0300

    Sezaam. New preparation protocol.

--- Tasks/hard/sezaaaam/create_dir.sh
+++ Tasks/hard/sezaaaam/create_dir.sh
@@ -1,21 +1,22 @@
 #!/usr/bin/bash
 
 # Maximum Number of levels
-LEVELS=3
+LEVELS=10
 # Number of folders on each level
 DIR_PER_LEVEL=10
 
+FLAG="kaiCTF{Deeep_f0resT_Is_@nnoying}"
+
+
+
 # First parameter - deep of current level
 # Second parameter - parent directory
-
 function create_dirs_on_level {
 
 # Identify current level of deep
 CUR_LEVEL=$1
 NEXT_LEVEL=$((CUR_LEVEL+1))
 
-
-#echo Currently at $CUR_LEVEL
 
 # If not reached the end of the recurrent
 if [ $NEXT_LEVEL -lt $LEVELS  ]
@@ -29,16 +30,25 @@ then
         create_dirs_on_level $NEXT_LEVEL $2/$d
     done;
 
-
-
 else
-    echo $2/$d
+    #FOR DEBUG ONLY
+    #echo $2/$d
     return
 fi
 
 }
 
+# Make folder to store temporary results
 mkdir nginx-dirs
 
+# Execute recursively
 create_dirs_on_level 0 nginx-dirs
 
+
+# Insert flag
+deepest_folder=$(find nginx-dirs -type d -links 2 -prune | awk '{print length, $0}' | sort -n | cut -d " " -f2- | tail -n1)
+
+touch $deepest_folder/flag
+
+echo $FLAG > $deepest_folder/flag
+echo Flag is on $deepest_folder/flag

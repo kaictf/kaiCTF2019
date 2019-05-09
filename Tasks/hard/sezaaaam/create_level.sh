2bac2de578d59cb08dd81c8f61b5b17733fb5588
Kirill Kukarcev <kirillkukarcev@gmail.com>
2019-05-09 18:47:03 +0300

    Sezaam. Changed coding and fixed password generation.

--- Tasks/hard/sezaaaam/create_level.sh
+++ Tasks/hard/sezaaaam/create_level.sh
@@ -9,13 +9,9 @@ mkdir levels
 # Enumerate over all levels
 for r in $(seq $LEVELS);
 do
-    PASS=$(shuf -n 1 $PASSFILE)
+    PASS=$(shuf -n 1 $PASSFILE | tr -d "\r\n" | cut -f1)
     USER=$PASS
 
-    #echo $PASS $USER
-
-    htpasswd -bc levels/level$r $USER $PASS
-
-    #echo "htpasswd -c levels/level$r $USER $PASS"
+    htpasswd -bc levels/level$r $USER $PASS;
 
 done;

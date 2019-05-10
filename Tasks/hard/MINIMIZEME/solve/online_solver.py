6787df353b250e1ba0b561cca8f7b69337e8824b
Kirill Kukarcev <kirillkukarcev@gmail.com>
2019-05-10 13:07:05 +0300

    HARD.MINIMIZEME. Auto-solvers and visualization playbook was added.

---
+++ Tasks/hard/MINIMIZEME/solve/online_solver.py
@@ -0,0 +1,39 @@
+import requests
+import numpy as np
+from scipy.optimize import least_squares,minimize,fmin_cg
+
+
+
+diff_link = "http://192.168.93.226:8080/diff/{0:.10f}/{0:.10f}"
+flag_link = "http://192.168.93.226:8080/try/{0:.10f}/{0:.10f}"
+
+def get_cost_evaluation(x_,y_):
+
+    actual_link= diff_link.format(x_,y_)
+    ans = requests.get(actual_link).json()
+
+    return ans['distance']
+
+def get_flag_for_position(x_,y_):
+
+    actual_link = flag_link.format(x_, y_)
+    ans = requests.get(actual_link).json()
+
+    return ans
+
+def get_c_eval_wrapper(x):
+    cost = get_cost_evaluation(x[0],x[1])
+    #print(x, cost)
+    return cost
+
+
+eta0 = np.array([np.random.random(),np.random.random() ])
+
+#minimum_parameters = minimize(get_c_eval_wrapper, eta0, method='Nelder-Mead', tol=1e-6)
+minimum_parameters = minimize(get_c_eval_wrapper, eta0, tol=1e-6)['x']
+#fmin_cg
+
+flag = get_flag_for_position(*minimum_parameters)
+
+print(flag)
+print(minimum_parameters)

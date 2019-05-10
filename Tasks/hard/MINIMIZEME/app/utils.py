c34dc746299b0fd6443323cbd9574afe6533afaf
Kirill Kukarcev <kirillkukarcev@gmail.com>
2019-05-10 13:06:36 +0300

    HARD.MINIMIZEME. New directory hierarchy.

---
+++ Tasks/hard/MINIMIZEME/app/utils.py
@@ -0,0 +1,31 @@
+from model import *
+
+flag = "kaiCTF2019{m@chinE_Le@RN1nG_Is_c00l}"
+wrong = "Too far. Try again"
+threshhold = 0.05
+
+def evaluate_cost_func(x_coord, y_coord):
+
+    input_x = np.array([x_coord,y_coord])
+
+    distance = cost_function_wrapper(input_x)
+
+    return {"distance": distance}
+
+
+def try_for_flag(x_coord, y_coord):
+    user_z_coord = f(x_coord,y_coord)
+
+    message = {"msg":"Blank answer"}
+
+    # If user has found the coordinates for global minimum
+    if np.abs(user_z_coord -z_min) <= threshhold:
+        message['msg'] = flag
+    else:
+        message['msg'] = wrong
+        message['poss'] = user_z_coord
+
+    return message
+
+
+
